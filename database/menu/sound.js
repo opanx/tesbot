2fe6bbc41299234055ed95fb01af3a57081af06a
rynz01 <78792626+rynz01@users.noreply.github.com>
2021-03-02 18:25:54 +0700

    Add files via upload

---
+++ database/menu/sound.js
@@ -0,0 +1,30 @@
+const sound = (pushname, prefix, botName, ownerName, getLevelingLevel, sender, _registered) => {
+	return `🔰 -----[ *SOUND MENU* ]----- 🔰
+Hallo, ${pushname} 👋
+Semoga harimu Menyenangkan User, ${sender.split("@")[0]}
+┏━━━━━━━━━━━━━━━━━━━━┓
+┃╭───────────────────
+┃│> Nama : ${pushname}
+┃│> Level : ${getLevelingLevel(sender)}
+┃│> User ${botName} : ${_registered.length}
+┃╰───────────────────
+┗━━━━━━━━━━━━━━━━━━━━┛
+┏━━━━━━━━━━━━━━━━━━━━┓
+┃╭───────────────────
+┃│> *${prefix}iri*
+┃│> *${prefix}pale*
+┃│> *${prefix}sound1*
+┃│> *${prefix}sound2*
+┃│> *${prefix}sound3*
+┃│> *${prefix}sound4*
+┃│> *${prefix}sound5*
+┃│> *${prefix}sound6*
+┃│> *${prefix}sound7*
+┃│> *${prefix}sound8*
+┃│> *${prefix}sound9*
+┃│> *${prefix}sound10*
+┃╰───────────────────
+┗━━━━━━━━━━━━━━━━━━━━┛
+🔰 -----[ *POWERED BY ${ownerName}* ]----- 🔰`
+}
+exports.sound = sound
